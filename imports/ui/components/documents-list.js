ea1613001206248d9b3ae557b9211b3a4ec97faf
tmcdeveloper <developer@themeteorchef.com>
2016-07-05 23:00:00 -0500

    add snippet demo code

--- imports/ui/components/documents-list.js
+++ imports/ui/components/documents-list.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { ListGroup, Alert } from 'react-bootstrap';
 import { Document } from './document.js';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export const DocumentsList = ({ documents }) => (
-  documents.length > 0 ? <ListGroup className="documents-list">
-    {documents.map((doc) => (
-      <Document key={ doc._id } document={ doc } />
-    ))}
-  </ListGroup> :
-  <Alert bsStyle="warning">No documents yet.</Alert>
+  <ListGroup className="documents-list">
+    <ReactCSSTransitionGroup
+      transitionName="documents-list"
+      transitionEnterTimeout={0}
+      transitionLeaveTimeout={0}
+    >
+      {documents.map((doc) => (
+        <Document key={ doc._id } document={ doc } />
+      ))}
+    </ReactCSSTransitionGroup>
+  </ListGroup>
 );
 
 DocumentsList.propTypes = {
